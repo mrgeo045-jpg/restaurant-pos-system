2ea077ec93a92d95b9987422c50e587896dc7b65
mrgeo045-jpg <mrgeo045@gmail.com>
2025-12-28 09:48:51 +0200

    Fix: Simplify component exports to resolve import path ambiguity

--- components/index.ts
+++ components/index.ts
@@ -2,6 +2,6 @@
 export * from './ui/button';
 export * from './ui/alert';
 
-// Re-export composed components
-export { default as Button } from './Button';
-export { default as Modal } from './Modal';
+// Re-export composed components - Direct imports to avoid ambiguous paths
+export { Button } from './ui/button';
+export { Modal } from './ui/Modal';
