61717f7ae1280bd51da97648e0ba4a29bf975c91
mrgeo045-jpg <mrgeo045@gmail.com>
2025-12-28 08:56:24 +0200

    feat: Add components/index.ts for centralized exports

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+// Re-export all UI components
+export * from './ui/button';
+export * from './ui/alert';
+
+// Re-export composed components
+export { default as Button } from './Button';
+export { default as Modal } from './Modal';
